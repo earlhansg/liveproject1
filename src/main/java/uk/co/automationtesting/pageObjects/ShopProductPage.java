7bad8496d054b4ed33ad398e3cbe7e84b89b06db
Earl Hans Genoso <earlhansg@gmail.com>
2022-12-16 06:12:40 +0800

    updated classes under pageObjects directory

--- src/main/java/uk/co/automationtesting/pageObjects/ShopProductPage.java
+++ src/main/java/uk/co/automationtesting/pageObjects/ShopProductPage.java
@@ -1,10 +1,14 @@
 package uk.co.automationtesting.pageObjects;
 
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class ShopProductPage {
+import uk.co.automationtesting.base.BasePage;
+
+public class ShopProductPage extends BasePage {
 
 	public WebDriver driver;
 	
@@ -14,27 +18,32 @@ public class ShopProductPage {
 	By addToCartBtn = By.cssSelector(".add-to-cart.btn.btn-primary");
 	By homepageLink = By.xpath("//span[.='Home']");
 	
-	public ShopProductPage(WebDriver driver) {
-		this.driver = driver;
+	public ShopProductPage() throws IOException {
+		super();
 	}
 	
-	public WebElement getSizeOption() {
+	public WebElement getSizeOption() throws IOException {
+		this.driver = getDriver();
 		return driver.findElement(sizeOption);
 	}
 	
-	public WebElement getQuantIncrease() {
+	public WebElement getQuantIncrease() throws IOException {
+		this.driver = getDriver();
 		return driver.findElement(quantityIncrease);
 	}
 
-	public WebElement getQuantDecrease() {
+	public WebElement getQuantDecrease() throws IOException {
+		this.driver = getDriver();
 		return driver.findElement(quantityDecrease);
 	}
 	
-	public WebElement getAddToCartBtn() {
+	public WebElement getAddToCartBtn() throws IOException {
+		this.driver = getDriver();
 		return driver.findElement(addToCartBtn);
 	}
 	
-	public WebElement getHomepageLink() {
+	public WebElement getHomepageLink() throws IOException {
+		this.driver = getDriver();
 		return driver.findElement(homepageLink);
 	}
 	
